4fb30112a9503e239c3a5d15db2f9cf45f94a6a3
arkmtech22 <113525666+arkmtech22@users.noreply.github.com>
2023-11-13 16:41:04 +0530

    Update Diabetes Prediction App6.py

--- pages/Diabetes Prediction App6.py
+++ pages/Diabetes Prediction App6.py
@@ -1,163 +1,112 @@
-import tkinter as tk
-from tkinter import ttk
+import streamlit as st
+import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.cluster import KMeans
+ # pip install scikit-fuzzy
+import numpy as np
+import skfuzzy as fuzz
+ # import skfuzzy as fuzz
+ # Load the diabetes dataset
+ # diabetes_df = pd.read_csv("/content/drive/My Drive/COLAB/diabetes.csv")
+diabetes_df = pd.read_csv(r'diabetes.csv')
 
-# Create the main application window
-app = tk.Tk()
-app.title("Diabetes Prediction App")
+ # Split the dataset into features and labels
+X = diabetes_df.iloc[:, :-1].values
+y = diabetes_df.iloc[:, -1].values
 
-# Function to create a slider with specified label, start, and end values
-def create_slider(parent, label, start, end):
-    label_widget = ttk.Label(parent, text=label)
-    label_widget.pack()
-    slider = ttk.Scale(parent, from_=start, to=end, orient="horizontal")
-    slider.pack()
-    value_label = ttk.Label(parent, text=f"{label}: {start}")  # Label to display the current value
-    value_label.pack()
-    
-    # Function to update the value label
-    def update_value(value):
-        value_label.config(text=f"{label}: {slider.get()}")
-    
-    # Bind the slider to the update function
-    slider.config(command=lambda value=slider: update_value(value))
+ # Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    return slider
+ # Feature scaling using the StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
-# Create sliders for user input based on the Pima Indian Diabetes dataset
-pregnancies_slider = create_slider(app, "Pregnancies", 0, 17)
-glucose_slider = create_slider(app, "Glucose", 0, 199)
-blood_pressure_slider = create_slider(app, "Blood Pressure", 0, 122)
-skinthickness_slider = create_slider(app, "Skin Thickness", 0, 99)  # Corrected slider name
-insulin_slider = create_slider(app, "Insulin", 0, 846)
-bmi_slider = create_slider(app, "BMI", 0, 67.1)
-dpf_slider = create_slider(app, "Diabetes Pedigree Function", 0.078, 2.42)
-age_slider = create_slider(app, "Age", 21, 81)
+ # Binary classification using logistic regression
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred_logreg = logreg.predict(X_test)
+acc_logreg = accuracy_score(y_test, y_pred_logreg)
+cr_logreg = classification_report(y_test, y_pred_logreg)
+cm_logreg = confusion_matrix(y_test, y_pred_logreg)
 
-# Create radio buttons for selecting "Diabetes" or "No Diabetes"
-diabetes_var = tk.StringVar()
-diabetes_var.set("Diabetes")  # Default selection
-diabetes_radio_button1 = ttk.Radiobutton(app, text="Diabetes", variable=diabetes_var, value="Diabetes")
-diabetes_radio_button2 = ttk.Radiobutton(app, text="No Diabetes", variable=diabetes_var, value="No Diabetes")
-diabetes_radio_button1.pack()
-diabetes_radio_button2.pack()
+ # Multiclass classification using decision tree
+dt = DecisionTreeClassifier()
+dt.fit(X_train, y_train)
+y_pred_dt = dt.predict(X_test)
+acc_dt = accuracy_score(y_test, y_pred_dt)
+cr_dt = classification_report(y_test, y_pred_dt)
+cm_dt = confusion_matrix(y_test, y_pred_dt)
 
-# Define empty lists to store user inputs
-feature_data = []
-labels = []
+ # Rule-based classification using a fuzzy expert system
+def get_fuzzy_expert_system(X_train, y_train, X_test):
+    # Divide the output space into three fuzzy sets
+    diabetes = fuzz.trimf([0, 0, 1], [0, 0.5, 1])
+    prediabetes = fuzz.trimf([0, 1, 2], [0.5, 1, 1.5])
+    normal = fuzz.trimf([1, 1, 2], [1, 1.5, 2])
 
-def collect_data():
-    # Get user input values from sliders and radio buttons
-    glucose = glucose_slider.get()
-    insulin = insulin_slider.get()
-    bp = blood_pressure_slider.get()
-    bmi = bmi_slider.get()
-    dpf = dpf_slider.get()
-    age = age_slider.get()
-    pregnancies = pregnancies_slider.get()
-    skinthickness = skinthickness_slider.get()
-    
-    # Convert the selected label to a binary value (0 for No Diabetes, 1 for Diabetes)
-    label = diabetes_var.get()
-    
-    # Append the user inputs and label to the respective lists
-    feature_data.append([glucose, insulin, bp, bmi, dpf, age, pregnancies, skinthickness])
-    labels.append(label)
-    
-    # Reset sliders and radio button
-    glucose_slider.set(0)
-    insulin_slider.set(0)
-    blood_pressure_slider.set(0)
-    bmi_slider.set(0)
-    dpf_slider.set(0)
-    age_slider.set(0)
-    pregnancies_slider.set(0)
-    skinthickness_slider.set(0)
-    diabetes_var.set("Diabetes" if diabetes_var.get() == "No Diabetes" else "No Diabetes")
-### Function to collect user data
-##def collect_data():
-##    # Get user input values from sliders and radio buttons
-##    glucose = glucose_slider.get()
-##    insulin = insulin_slider.get()
-##    bp = blood_pressure_slider.get()
-##    bmi = bmi_slider.get()
-##    dpf = dpf_slider.get()
-##    age = age_slider.get()
-##    pregnancies = pregnancies_slider.get()
-##    skinthickness = skinthickness_slider.get()
-##    
-##    # Convert the selected label to a binary value (0 for No Diabetes, 1 for Diabetes)
-##    label = 1 if diabetes_var.get() == "Diabetes" else 0
-##
-##    # Append the user inputs and label to the respective lists
-##    feature_data.append([glucose, insulin, bp, bmi, dpf, age, pregnancies, skinthickness])
-##    labels.append(label)
-##
-##    # Reset sliders and radio button
-##    glucose_slider.set(0)
-##    insulin_slider.set(0)
-##    blood_pressure_slider.set(0)
-##    bmi_slider.set(0)
-##    dpf_slider.set(0)
-##    age_slider.set(0)
-##    pregnancies_slider.set(0)
-##    skinthickness_slider.set(0)
-##    diabetes_var.set("Diabetes")
+     # Define the fuzzy rules
+    rule1 = np.fmax(diabetes, normal)
+    rule2 = prediabetes
+    rule3 = np.fmax(prediabetes, diabetes)
 
-# Function to predict diabetes
-def predict_diabetes():
-    if len(feature_data) < 2:
-        result_label.config(text="Collect more data points for splitting and prediction.")
-        return
-    
-    # Split the dataset into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(feature_data, labels, test_size=0.2, random_state=42)
+     # Combine the rules using the OR operator
+    or_rule = np.fmax(rule1, np.fmax(rule2, rule3))
 
-    # Train a simple Logistic Regression model (you can replace this with other algorithms)
-    models = {
-        "Logistic Regression": LogisticRegression(),
-        "Decision Tree": DecisionTreeClassifier(),
-        "Random Forest": RandomForestClassifier(),
-        "SVM": SVC(),
-        "K-Nearest Neighbors": KNeighborsClassifier()
-    }
+     # Compute the output using the weighted average defuzzification method
+    y_train_fuzzy = np.column_stack([fuzz.interp_membership(X_train[:, i], X_train[:, i], or_rule[i]) for i in range(len(X_train[0]))])
+    y_test_fuzzy = np.column_stack([fuzz.interp_membership(X_test[:, i], X_test[:, i], or_rule[i]) for i in range(len(X_test[0]))])
+    y_pred_fuzzy = np.argmax(y_train_fuzzy, axis=1)
+    y_pred_fuzzy_test = np.argmax(y_test_fuzzy, axis=1)
 
-    results = {}
-    for model_name, model in models.items():
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        results[model_name] = accuracy
+    return y_pred_fuzzy_test
+y_pred_fuzzy = get_fuzzy_expert_system(X_train, y_train, X_test)
+acc_fuzzy = accuracy_score(y_test, y_pred_fuzzy)
+cr_fuzzy = classification_report(y_test, y_pred_fuzzy)
+cm_fuzzy = confusion_matrix(y_test, y_pred_fuzzy)
 
-    # Plot the prediction results
-    plt.bar(range(len(results)), list(results.values()), align='center')
-    plt.xticks(range(len(results)), list(results.keys()), rotation='vertical')
-    plt.ylabel('Accuracy')
-    plt.title('Diabetes Prediction Results')
-    plt.show()
+ # Fuzzy logic-based classification using fuzzy c-means clustering
+def get_fuzzy_cmeans(X_train, y_train, X_test):
+     # Use the elbow method to determine the number of clusters
+    wcss = []
+    for i in range(1, 11):	
+        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        kmeans.fit(X_train)
+        wcss.append(kmeans.inertia_)
+    st.line_chart(wcss)
+    k = st.slider('Select number of clusters for fuzzy c-means:', min_value=1, max_value=10)
+    cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(X_train.T, k, 2, error=0.005, maxiter=1000, init=None)
+    u_pred = fuzz.cluster.cmeans_predict(X_test.T, cntr, 2, error=0.005, maxiter=1000)
+    y_pred_fcm = np.argmax(u_pred, axis=0)
+    return y_pred_fcm
 
-    # Clear feature_data and labels
-    feature_data.clear()
-    labels.clear()
+y_pred_fcm = get_fuzzy_cmeans(X_train, y_train, X_test)
+y_pred_fuzzy_test = np.argmax(y_pred_fcm, axis=0)
+acc_fcm = accuracy_score(y_test, y_pred_fuzzy_test)
+cr_fcm = classification_report(y_test, y_pred_fuzzy_test)
+cm_fcm = confusion_matrix(y_test, y_pred_fuzzy_test)
 
-# Button to collect data
-collect_data_button = ttk.Button(app, text="Collect Data", command=collect_data)
-collect_data_button.pack()
-
-# Button to predict diabetes
-predict_button = ttk.Button(app, text="Predict Diabetes", command=predict_diabetes)
-predict_button.pack()
-
-# Label to display the prediction results
-result_label = ttk.Label(app, text="")
-result_label.pack()
-
-# Start the main event loop
-app.mainloop()
+ # Display the results
+st.write('## Diabetes Prediction with Supervised Machine Learning')
+st.write('### Binary Classification using Logistic Regression')
+st.write('Accuracy:', acc_logreg)
+st.write('Classification Report:\n', cr_logreg)
+st.write('Confusion Matrix:\n', cm_logreg)
+st.write('### Multiclass Classification using Decision Tree')
+st.write('Accuracy:', acc_dt)
+st.write('Classification Report:\n', cr_dt)
+st.write('Confusion Matrix:\n', cm_dt)
+st.write('### Rule-based Classification using a Fuzzy Expert System')
+st.write('Accuracy:', acc_fuzzy)
+st.write('Classification Report:\n', cr_fuzzy)
+st.write('Confusion Matrix:\n', cm_fuzzy)
+st.write('### Fuzzy Logic-based Classification using Fuzzy C-means Clustering')
+st.write('Accuracy:', acc_fcm)
+st.write('Classification Report:\n', cr_fcm)
+st.write('Confusion Matrix:\n', cm_fcm)
+            

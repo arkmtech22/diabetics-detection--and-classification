018147fbc16b2572dd6a29c32f5db3e2f9cff4ab
arkmtech22 <113525666+arkmtech22@users.noreply.github.com>
2023-12-03 20:34:59 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,65 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
 
-LOGGER = get_logger(__name__)
+# Load Pima dataset
+df = pd.read_csv('diabetes.csv')
 
+# Train the classifier
+X = df.drop('Outcome', axis=1)
+y = df['Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Streamlit app
+st.title("Diabetes Detection Web App")
 
-    st.write("# Welcome to Streamlit! 👋")
+# Sidebar with sliders
+pregnancies = st.sidebar.slider('Number of Pregnancies', 0, 20, 1)
+glucose = st.sidebar.slider('Glucose Level', 0, 200, 100)
+blood_pressure = st.sidebar.slider('Blood Pressure', 0, 150, 70)
+skin_thickness = st.sidebar.slider('Skin Thickness', 0, 99, 20)
+insulin = st.sidebar.slider('Insulin Level', 0, 846, 79)
+bmi = st.sidebar.slider('BMI', 0.0, 67.1, 25.0)
+diabetes_pedigree = st.sidebar.slider('Diabetes Pedigree Function', 0.078, 2.42, 0.3725)
+age = st.sidebar.slider('Age', 21, 81, 30)
 
-    st.sidebar.success("Select a demo above.")
+# Make prediction
+if st.button('Predict'):
+    user_input = pd.DataFrame({
+        'Pregnancies': [pregnancies],
+        'Glucose': [glucose],
+        'BloodPressure': [blood_pressure],
+        'SkinThickness': [skin_thickness],
+        'Insulin': [insulin],
+        'BMI': [bmi],
+        'DiabetesPedigreeFunction': [diabetes_pedigree],
+        'Age': [age]
+    })
+    prediction = clf.predict(user_input)
+    st.write(f"Prediction: {'Diabetes' if prediction[0] == 1 else 'No Diabetes'}")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# Evaluation button
+if st.button('Evaluate Model'):
+    y_pred = clf.predict(X_test)
+    cm = confusion_matrix(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, clf.predict_proba(X_test)[:, 1])
 
+    st.write("Confusion Matrix:")
+    st.write(cm)
+    st.write("Classification Report:")
+    st.write(report)
+    st.write(f"Accuracy: {accuracy:.4f}")
+    st.write(f"Precision: {precision:.4f}")
+    st.write(f"Recall: {recall:.4f}")
+    st.write(f"F1 Score: {f1:.4f}")
+    st.write(f"ROC-AUC: {roc_auc:.4f}")
 
-if __name__ == "__main__":
-    run()
